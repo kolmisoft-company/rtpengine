8a9c8cd6c7ac90114718954eb96e4229bffb8b84
kolmisoft-company <87749936+kolmisoft-company@users.noreply.github.com>
2021-09-21 12:09:16 +0300

    Use _dup for codec item

--- daemon/call_interfaces.c
+++ daemon/call_interfaces.c
@@ -1691,20 +1691,21 @@ static void ng_stats_media(bencode_item_t *list, const struct call_media *m,
 	bencode_item_t *dict, *streams = NULL, *flags;
 	GList *l;
 	struct packet_stream *ps;
+	onst struct rtp_payload_type *rtp_pt = NULL;
 
 	if (!list)
 		goto stats;
 
 	dict = bencode_list_add_dictionary(list);
 	
-	const struct rtp_payload_type *rtp_pt = __rtp_stats_codec((struct call_media *)m);
+	rtp_pt = __rtp_stats_codec((struct call_media *)m);
 
 	bencode_dictionary_add_integer(dict, "index", m->index);
 	bencode_dictionary_add_str(dict, "type", &m->type);
 	if (m->protocol)
 		bencode_dictionary_add_string(dict, "protocol", m->protocol->name);
 	if (rtp_pt)
-		bencode_dictionary_add_str(dict, "codec", &rtp_pt->encoding_with_params);
+		bencode_dictionary_add_str_dup(dict, "codec", &rtp_pt->encoding_with_params);
 
 	streams = bencode_dictionary_add_list(dict, "streams");
 
